54766683ecf230923bf0a6347377617e87e930f3
vishu221b <vishal.dogra738@gmail.com>
2020-07-30 21:30:33 +0530

    Added validation callbacks

--- db/items.js
+++ db/items.js
@@ -9,6 +9,39 @@ const pool = new Pool({
 });
 
 
+let checkIfUserExistsForId = (itemId, callback)=>{
+    pool.query('Select * from items where id=$1', [itemId], (error, result)=>{
+        if(error){
+            throw error;
+        }else if (result.rows.length > 0){
+            callback({error: false, result: result.rows});
+        }else{
+            callback({error: true, result: `No item found with id ${itemId}`});
+        }
+    });
+};
+
+let checkIfItemNameAndModelExists = (nameModel, callback)=>{
+    if (nameModel.length !== 2){
+        callback({error:true, result: "Both item name and model are required."});
+    }
+    else{
+        pool.query('select * from items where name=$1 and model=$2',nameModel ,(error, result) => {
+            if (error){
+                throw error;
+            }
+            else{
+                if (result.rowCount >= 1 ){
+                    callback({error: true, result: "Cannot create item as an item with same name and model already exists."});
+                }
+                else if (result.rowCount === 0){
+                    callback({error: false});
+                }
+            }
+        });
+    }
+};
+
 let getAllItems = (request, response) =>{
     pool.query('Select * from items', (error, result) => {
         if (error){
@@ -25,64 +58,78 @@ let getAllItems = (request, response) =>{
 
 let getSingleItem = (request, response) => {
     let itemId = request.params.id;
-    pool.query('Select * from items where id=$1', [itemId], (error, result) => {
-        if (error){
-            throw error;
+    checkIfUserExistsForId(itemId, (result)=>{
+        if (result.error){
+            response.status(404).json({response: result.result});
         }
         else{
-            response.status(200).json({response: result.rows});
+            response.status(200).json({response: result.result});
         }
     });
 };
 
 let createNewItem = (request, response) => {
     let { name, model, price } = request.body;
-    if (!name){
-        response.status(400).json({error: "Please specify the name of the item."});
-    }
-    else if (!model){
-        response.status(400).json({error: "Please specify the model of the item."});
-    }
-    else if (!price){
-        response.status(400).json({error: "Please specify the price of the item."});
-    }
-    else{
-        let currentTimestamp = new Date();
-        pool.query(`Insert into items (name, model, price, created_at, last_updated_at, is_active) values ($1, $2, $3, $4, $5, $6)`,
-        [name, model, price, currentTimestamp, currentTimestamp, 1],
-        (error, result)=>{
-            if (error){
-                throw error;
+    checkIfItemNameAndModelExists([name, model], (res) => {
+        if (res.error){
+            response.status(400).json({response: res.result});
+        }
+        else{
+            if (!name){
+                response.status(400).json({error: "Please specify the name of the item."});
             }
-            response.status(201).json({ response: "SUCCESS", message: `Created ${result.rowCount} items successfully.`});
-        });
-    }
+            else if (!model){
+                response.status(400).json({error: "Please specify the model of the item."});
+            }
+            else if (!price){
+                response.status(400).json({error: "Please specify the price of the item."});
+            }
+            else{
+                let currentTimestamp = new Date();
+                pool.query(`Insert into items (name, model, price, created_at, last_updated_at, is_active) values ($1, $2, $3, $4, $5, $6)`,
+                [name, model, price, currentTimestamp, currentTimestamp, 1],
+                (error, result)=>{
+                    if (error){
+                        throw error;
+                    }
+                    response.status(201).json({ response: "SUCCESS", message: `Created ${result.rowCount} items successfully.`});
+                });
+            }
+        }
+    });
 };
 
 let updateExistingItem = (request, response) => {
     let itemId = request.params.id;
-    let { name, model, price } = request.body;
-    if (!(name.length >= 3)){
-        response.status(400).json({error: "Invalid item name length. Minimum length should be 3."});
-    }
-    else if (!(model.length >= 2)){
-        response.status(400).json({error: "Invalid item model length. Minimum length should be 2."});
-    }
-    else if(!price){
-        model.price = 100;
-    }
-    else{
-        let currentTimestamp = new Date();
-        console.log([name, model, itemId, price]);
-        pool.query("UPDATE items set name=$1, model=$2, price=$4, last_updated_at=$5 where id=$3", [name, model, itemId, price, currentTimestamp], (error, result)=>{
-            if(error){
-                throw error;
+    checkIfUserExistsForId(itemId, (res) => {
+        if(res.error){
+            response.status(404).json({response: res.result});
+        }
+        else{
+            let { name, model, price } = request.body;
+            if (!(name.length >= 3)){
+                response.status(400).json({error: "Invalid item name length. Minimum length should be 3."});
+            }
+            else if (!(model.length >= 2)){
+                response.status(400).json({error: "Invalid item model length. Minimum length should be 2."});
+            }
+            else if(!price){
+                model.price = 100;
             }
             else{
-                response.status(200).json({response: "SUCCESS", message: "Item updated successfully."});
+                let currentTimestamp = new Date();
+                console.log([name, model, itemId, price]);
+                pool.query("UPDATE items set name=$1, model=$2, price=$4, last_updated_at=$5 where id=$3", [name, model, itemId, price, currentTimestamp], (error, result)=>{
+                    if(error){
+                        throw error;
+                    }
+                    else{
+                        response.status(200).json({response: "SUCCESS", message: "Item updated successfully."});
+                    }
+                });
             }
-        });
-    }
+        }
+    });
 };
 
 module.exports = {
